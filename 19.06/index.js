a62f4b79cbb279f7d34a9d0e3b5a40fa26b92754
Lisandra Cruz <lisansouza@gmail.com>
2023-06-20 19:53:59 -0300

    The last question of exercises

--- 19.06/index.js
+++ 19.06/index.js
@@ -60,14 +60,82 @@
 // converterEmHorasEMinutos(3600);
 
 // 4. Crie uma função que receba três números, verifique e mostre o maior deles.
-const verificaMaiorNumero = (numero1, numero2, numero3) => {
-  if (numero1 > numero2 && numero1 > numero3) {
-    console.log("O numero " + numero1 + " é o maior entre os 3");
-  } else if (numero2 > numero3 && numero2 > numero1) {
-    console.log("O numero " + numero2 + " é o maior entre os 3");
-  } else if (numero3 > numero1 && numero3 > numero2) {
-    console.log("O numero " + numero3 + " é o maior entre os 3");
+// const verificaMaiorNumero = (numero1, numero2, numero3) => {
+//   if (numero1 > numero2 && numero1 > numero3) {
+//     console.log("O numero " + numero1 + " é o maior entre os 3");
+//   } else if (numero2 > numero3 && numero2 > numero1) {
+//     console.log("O numero " + numero2 + " é o maior entre os 3");
+//   } else if (numero3 > numero1 && numero3 > numero2) {
+//     console.log("O numero " + numero3 + " é o maior entre os 3");
+//   }
+// };
+
+// verificaMaiorNumero(33, 23, -7);
+
+// 5. Crie uma função que receba um número e usando laços de repetição calcule e mostre a tabuada desse número
+
+// 1. Criar uma função que recebe um paramentro OK
+// 2. Utilizar uma estrutura de repetição (for ou while) para calcular
+
+const mostrarTabuada = (numero) => {
+  // console.log(numero * 1);
+  // console.log(numero * 2);
+  // console.log(numero * 3);
+  // console.log(numero * 4);
+  // console.log(numero * 5);
+  // console.log(numero * 6);
+  // console.log(numero * 7);
+  // console.log(numero * 8);
+  // console.log(numero * 9);
+  // console.log(numero * 10);
+
+  for (let i = 1; i <= 10; i++) {
+    console.log(numero + " * " + i + " = " + numero * i);
   }
 };
 
-verificaMaiorNumero(33, 23, -7);
+mostrarTabuada(25);
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i + 1);
+// }
+
+// // Rodou 1
+// i= 1
+// console: 2
+
+// // Rodou 2
+// i = 2
+// console: 3
+
+// // Rodou 3
+// i = 3
+// console: 4
+
+// // Rodou 4
+// i = 4
+// console: 5
+
+// // Rodou 5
+// i = 5
+// console: 6
+
+// // Rodou 6
+// i = 6
+// console: 7
+
+// // Rodou 7
+// i = 7
+// console: 8
+
+// // Rodou 8
+// i = 8
+// console: 9
+
+// // Rodou 9
+// i = 9
+// console: 10
+
+// // Rodou 10
+// i = 10
+// console: 11
